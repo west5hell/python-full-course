5f2a247050a3f6638c6373d41a588329fcf75d94
Ponter Chia <dozeuto@gmail.com>
2025-05-04 15:32:47 +0800

    Add weight_converter.py for converting between kilograms and pounds

---
+++ weight_converter.py
@@ -0,0 +1,13 @@
+# Python weight converter
+
+weight = float(input("Enter your weight: "))
+unit = input("Kilograms or Pounds? (K or L): ")
+
+if unit == "K":
+    weight = weight * 2.205
+    unit = "Lbs."
+elif unit == "L":
+    weight = weight / 2.205
+    unit = "Kgs."
+else:
+    print(f"{unit} was not valid")
