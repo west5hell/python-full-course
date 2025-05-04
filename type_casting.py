bdbe0178614e710620429dc12048ccc9d89da713
Ponter Chia <dozeuto@gmail.com>
2025-05-04 15:38:04 +0800

    Remove unused files: type_casting.py, user_input.py, and variables.py

--- type_casting.py
+++
@@ -1,7 +0,0 @@
-# Typecasting = the process of converting a variable from one data type to another
-#           str(), int(), float(), bool()
-
-name = "John Doe"
-age = 33
-gpa = 3.4
-is_student = True
