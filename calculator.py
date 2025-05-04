7cfb543babda4b4ae9f3164c587b69bed9c85114
Ponter Chia <dozeuto@gmail.com>
2025-05-04 15:28:09 +0800

    Add calculator.py for basic arithmetic operations with user input

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# Python calculator
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2
+else:
+    print(f"{operator} is not valid operator")
+
+print(f"Result is {round(result, 3)}")
