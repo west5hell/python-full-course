c430a356c9134e9318d2d6b946723baad450359d
Ponter Chia <dozeuto@gmail.com>
2025-05-10 20:44:06 +0800

    Refactor MainWindow class to improve UI layout and add color-coded labels

--- py_qt.py
+++ py_qt.py
@@ -1,8 +1,17 @@
 # PyQt5
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel,
+    QWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QGridLayout,
+)
+
+google_logo = "google_logo-google_icongoogle-512.webp"
 
 
 class MainWindow(QMainWindow):
@@ -10,7 +19,33 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("My cool first GUI")
         self.setGeometry(700, 300, 500, 500)
-        self.setWindowIcon(QIcon("google_logo-google_icongoogle-512.webp"))
+        self.initUI()
+
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        label1 = QLabel("#1", self)
+        label2 = QLabel("#2", self)
+        label3 = QLabel("#3", self)
+        label4 = QLabel("#4", self)
+        label5 = QLabel("#5", self)
+
+        label1.setStyleSheet("background-color: red;")
+        label2.setStyleSheet("background-color: yellow;")
+        label3.setStyleSheet("background-color: green;")
+        label4.setStyleSheet("background-color: blue;")
+        label5.setStyleSheet("background-color: purple;")
+
+        vbox = QGridLayout()
+
+        vbox.addWidget(label1, 0, 0)
+        vbox.addWidget(label2, 0, 1)
+        vbox.addWidget(label3, 1, 0)
+        vbox.addWidget(label4, 1, 1)
+        vbox.addWidget(label5, 1, 2)
+
+        central_widget.setLayout(vbox)
 
 
 def main():
