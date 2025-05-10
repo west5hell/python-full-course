ffe2d4562d64603fc4ee63199a97f4c2476423c6
Ponter Chia <dozeuto@gmail.com>
2025-05-10 20:51:24 +0800

    Refactor MainWindow class to simplify layout and remove unused widgets

--- py_qt.py
+++ py_qt.py
@@ -1,15 +1,7 @@
 # PyQt5
 
 import sys
-from PyQt5.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QLabel,
-    QWidget,
-    QVBoxLayout,
-    QHBoxLayout,
-    QGridLayout,
-)
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 
 google_logo = "google_logo-google_icongoogle-512.webp"
 
@@ -19,33 +11,20 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("My cool first GUI")
         self.setGeometry(700, 300, 500, 500)
+        self.button = QPushButton("Click me!", self)
+        self.label = QLabel("Hello", self)
         self.initUI()
 
     def initUI(self):
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+        self.button.setGeometry(150, 200, 200, 100)
+        self.button.setStyleSheet("font-size:30px;")
+        self.button.clicked.connect(self.on_click)
 
-        label1 = QLabel("#1", self)
-        label2 = QLabel("#2", self)
-        label3 = QLabel("#3", self)
-        label4 = QLabel("#4", self)
-        label5 = QLabel("#5", self)
+        self.label.setGeometry(150, 300, 200, 100)
+        self.label.setStyleSheet("font-size:50px;")
 
-        label1.setStyleSheet("background-color: red;")
-        label2.setStyleSheet("background-color: yellow;")
-        label3.setStyleSheet("background-color: green;")
-        label4.setStyleSheet("background-color: blue;")
-        label5.setStyleSheet("background-color: purple;")
-
-        vbox = QGridLayout()
-
-        vbox.addWidget(label1, 0, 0)
-        vbox.addWidget(label2, 0, 1)
-        vbox.addWidget(label3, 1, 0)
-        vbox.addWidget(label4, 1, 1)
-        vbox.addWidget(label5, 1, 2)
-
-        central_widget.setLayout(vbox)
+    def on_click(self):
+        self.label.setText("Goodbye")
 
 
 def main():
